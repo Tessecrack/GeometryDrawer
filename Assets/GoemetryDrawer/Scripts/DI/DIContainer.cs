eb5add75d1e1f61233e391085afa31dd82b9c907
Tessecrack <deputamandre441@gmail.com>
2024-10-25 15:45:05 +0300

    added project

---
+++ Assets/GoemetryDrawer/Scripts/DI/DIContainer.cs
@@ -0,0 +1,9 @@
+
+namespace Assets.GoemetryDrawer.Scripts.DI
+{
+    public class DIContainer
+    {
+
+    }
+}
+
---
+++ Assets/GoemetryDrawer/Scripts/Root/Startup.cs
@@ -0,0 +1,12 @@
+
+namespace Assets.GoemetryDrawer.Scripts.Root
+{
+    public class Startup
+    {
+        private static Startup _instance;
+
+        private UIRoot _uiRoot; // monobeh
+
+
+    }
+}
